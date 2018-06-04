2f0f7139b00c664b05e1fe9d53da5997c0c1abfe
Ferdous-Al-Quraeshi <ecopavel@gmail.com>
2018-06-04 22:20:33 +0600

    webbeck config for production env (renamed file) & corrected 'start' script, added hot-loader module

--- webpack-dev.config.js
+++ webpack-dev.config.js
@@ -20,13 +20,18 @@ module.exports = {
   },
   resolve: { extensions: ['*', '.js', '.jsx'] },
   output: {
-    publicPath: bundlePath,
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
     filename: "bundle.js"
   },
   devServer: {
     contentBase: path.join(__dirname, 'public'),
     port: 3000,
-    publicPath: "http://localhost:3000/dist"
+    publicPath: "http://localhost:3000/dist",
+    hotOnly: true
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
